6f89aa6f3aa3355a980dbe581755c3a3cb73ddb8
Konner Hartman <konner.hartman@yahoo.com>
2022-01-05 14:36:44 -0600

    added memoji, but still needs formatting

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import computerEmoji from '../../images/kh-emoji-computer.png';
 
 export default function Home() {
   return (
-    <div className="col-lg-8">
-        <div id="mainLogo">
+    <div className="container rox">
+        <div className="col-lg-4">
+            <img className="d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block img-fluid rounded-circle" height="700px" src={computerEmoji} alt="Konner standing with hands in pockets and a smile on his face."/>
+        </div>
+        <div className="col-lg-8">
             <div className="my-5 mx-5 px-5 text-center">
                 <h2>FULL STACK WEB DEVELOPER</h2>
                 <p className="fw-bold py-0 my-0">Click any of the links in the navigation bar above to learn a little about me.</p>
