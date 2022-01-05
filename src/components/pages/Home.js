03ab745da67ddd4f2c36f977bef39cf26cd7c4d5
Konner Hartman <konner.hartman@yahoo.com>
2022-01-04 21:44:30 -0600

    added background image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,32 +1,23 @@
 import React from 'react';
-import ProfPic from '../../images/Prof-Pic-Square.jpg'
-import KHLogo from '../../images/KH-LOGO copy.png';
 
 export default function Home() {
   return (
-    <div className="row">
-        <section className="col-lg-4">
-            <img className="d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block img-fluid rounded-circle" height="700px" src={ProfPic} alt="Konner standing with hands in pockets and a smile on his face."/>
-        </section>
-
-        <div className="col-lg-8">
-            <div id="mainLogo">
-                <div className="my-5 mx-5 px-5 text-center">
-                    <img class="img-fluid d-block my-5" height="300px" src={KHLogo} alt="Konner Hartman's logo."/>
-                    <h2>FULL STACK WEB DEVELOPER</h2>
-                    <p className="fw-bold py-0 my-0">Click any of the links in the navigation bar above to learn a little about me.</p>
-                    <p className="fw-bold py-0 my-0">-OR-</p>
-                    <p className="fw-bold pt-0 mt-0">Let's get straight to business!</p>
-                    <button type="button" className="btn btn-outline-dark mx-2">
-                        <a target="_blank" rel="noreferrer" className="nav-link active text-reset" href="https://github.com/konnerhartman">Github</a>
-                    </button>
-                    <button type="button" className="btn btn-outline-dark mx-2">
-                        <a target="_blank" rel="noreferrer" className="nav-link active text-reset" href="https://www.linkedin.com/in/konner-hartman/">LinkedIn</a>
-                    </button>
-                    <button type="button" className="btn btn-outline-dark mx-2">
-                        <a target="_blank" rel="noreferrer" className="nav-link active text-reset" href="https://drive.google.com/file/d/1JEeGyZeOsCBbJvjaGcXqMN_F2mmo1N4F/view">Resume</a>
-                    </button>
-                </div>
+    <div className="col-lg-8">
+        <div id="mainLogo">
+            <div className="my-5 mx-5 px-5 text-center">
+                <h2>FULL STACK WEB DEVELOPER</h2>
+                <p className="fw-bold py-0 my-0">Click any of the links in the navigation bar above to learn a little about me.</p>
+                <p className="fw-bold py-0 my-0">-OR-</p>
+                <p className="fw-bold pt-0 mt-0">Let's get straight to business!</p>
+                <button type="button" className="btn btn-outline-light mx-2">
+                    <a target="_blank" rel="noreferrer" className="nav-link active text-reset" href="https://github.com/konnerhartman">Github</a>
+                </button>
+                <button type="button" className="btn btn-outline-light mx-2">
+                    <a target="_blank" rel="noreferrer" className="nav-link active text-reset" href="https://www.linkedin.com/in/konner-hartman/">LinkedIn</a>
+                </button>
+                <button type="button" className="btn btn-outline-light mx-2">
+                    <a target="_blank" rel="noreferrer" className="nav-link active text-reset" href="https://drive.google.com/file/d/1JEeGyZeOsCBbJvjaGcXqMN_F2mmo1N4F/view">Resume</a>
+                </button>
             </div>
         </div>
     </div>
