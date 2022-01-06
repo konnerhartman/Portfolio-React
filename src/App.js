5333b7d127fd3e31ec7519ecc7c67d2e956470da
Konner Hartman <konner.hartman@yahoo.com>
2022-01-06 14:51:06 -0600

    edded footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Footer from "./components/Footer";
 
-const App = () => <PortfolioContainer />;
+const styles = {
+    portBody: {
+         backgroundColor: '#ffffff10',
+         backdropFilter: 'blur(12px)',
+         
+    }
+}
+
+function App() { 
+  return (
+    <div>
+        <div className="shadow-lg rounded-3 m-5 mt-0" style={styles.portBody}>
+        <PortfolioContainer />
+        </div>
+        <Footer />
+    </div>
+  )
+};
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="sticky-bottom text-center mb-0">
+            <p>Created by Konner Hartman</p>
+        </footer>
+    )
+}
