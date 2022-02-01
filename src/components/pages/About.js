0218bce9f99a37bb1a24c35ec2f230bb137a1666
Konner Hartman <konner.hartman@yahoo.com>
2022-02-01 13:22:25 -0600

    updated about me paragraph

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,7 +13,7 @@ export default function About() {
                 <h2 className="fw-bold">About Me</h2>
                 <div>
                     <p className='mt-4'>An innovative problem-solver passionate about user experience, my current focus is on app functionality and development.</p>
-                    <p className='mt-4'>I am a Full Stack Web Developer with an emphasis on back end development, holding a Bachelor of Science From Texas A&M University. I will soon be receiving a certificate in Full Stack Development from Southern Methodist University, where I developed skills in JavaScript, MySQL, MongoDB, NodeJS, React.js, and more.</p>
+                    <p className='mt-4'>I am a Full Stack Web Developer with an emphasis on back end development, holding a Bachelor of Science From Texas A&M University. I recently received a certificate in Full Stack Development from Southern Methodist University, where I developed skills in JavaScript, MySQL, MongoDB, NodeJS, React.js, and more.</p>
                     <button type="button" className="aboutBtn btn btn-outline-light mt-4">
                         <a target="_blank" rel="noreferrer" className="nav-link active text-reset" href={resume}>Resume</a>
                     </button>
