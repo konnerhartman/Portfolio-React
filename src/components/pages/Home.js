7cda8d381a63652de1142ab8e3d6c4e5a170004b
Konner Hartman <konner.hartman@yahoo.com>
2022-02-02 14:05:04 -0600

    fixed text animation on homepage for mobile

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ import resume from '../../images/Konner_Hartman_Full_Resume.pdf';
 export default function Home() {
   return (
     <div className="container">
-        <div className="row my-4">
+        <div className="row">
             <div className="col-lg-8 col-md-8 col-sm-12">
                 <div className="animated-title">
                     <div className="text-top">
@@ -19,7 +19,7 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <aside className="col-lg-4 col-md-4 col-sm-12">
+            <aside className="mt-4 col-lg-4 col-md-4 col-sm-12">
                 <img className="d-none d-sm-none d-md-block d-lg-block img-fluid" src={waveEmoji} alt="Emoji of Konner waving his hand."/>
                 <div className="text-center">
                     <p className="fw-bold m-3">Let's get to business!</p>
